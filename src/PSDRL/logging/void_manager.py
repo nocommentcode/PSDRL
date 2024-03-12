4943a91281589ec3707d9b56c94c96d58f5a6aa1
Malo Hamon <hamon.malo@gmail.com>
2024-03-12 11:09:38 +0000

    logging bug fix

--- src/PSDRL/logging/void_manager.py
+++ src/PSDRL/logging/void_manager.py
@@ -8,5 +8,11 @@ class VoidManager(DataManager):
     def update(self, log: dict, timestep: int):
         pass
 
+    def log_images(self, name: str, images: list, timestep: int):
+        pass
+
+    def log_videos(self, name: str, video_frames: list, timestep: int):
+        pass
+
     def save(self, agent, timestep: int):
         pass
