208e639f68b4966a51a5737959f93604a7aa0a1b
Malo Hamon <hamon.malo@gmail.com>
2024-03-12 11:10:09 +0000

    pull up agent model functions

--- src/PSDRL/agent/neural_linear_agent_model.py
+++ src/PSDRL/agent/neural_linear_agent_model.py
@@ -10,41 +10,20 @@ from ..training.transition import TransitionModelTrainer
 
 class NeuralLinearAgentModel(AgentModel):
     def __init__(self, config: dict, device: str, actions: torch.tensor) -> None:
-        super().__init__(config, device)
-        terminal_network = TerminalNetwork(
-            config["representation"]["embed_dim"], config["terminal"], self.device
-        )
-
-        transition_network = TransitionNetwork(
-            config["representation"]["embed_dim"],
-            len(actions),
-            config["transition"],
-            self.device,
-        )
+        super().__init__(config, device, actions)
 
         self.model = NeuralLinearModel(
             config["algorithm"],
             config["representation"]["embed_dim"],
             actions,
-            transition_network,
-            terminal_network,
+            self.transition_network,
+            self.terminal_network,
             self.autoencoder,
-            self.device,
-        )
-
-        self.transition_trainer = TransitionModelTrainer(
-            config["transition"],
-            transition_network,
-            self.autoencoder,
-            terminal_network,
-            config["replay"]["batch_size"],
-            len(actions),
             self.device,
         )
 
     def train(self, dataset: Dataset):
-        self.representation_trainer.train_(dataset)
-        self.transition_trainer.train_(dataset)
+        super().train(dataset)
 
         self.model.update_posteriors(dataset)
         self.model.sample()
