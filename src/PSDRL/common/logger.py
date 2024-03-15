fcf8cc08797338b8feabefa8f754491a363670e2
Malo Hamon <hamon.malo@gmail.com>
2024-03-15 12:33:56 +0000

    sd

--- src/PSDRL/common/logger.py
+++ src/PSDRL/common/logger.py
@@ -4,50 +4,6 @@ if TYPE_CHECKING:
     from ..common.data_manager import DataManager
 
 
-class Logger:
-    def __init__(self, data_manager: "DataManager"):
-        self.log = {"scalars": {}, "timestep_scalars": {}}
-        self.data_manager = data_manager
-
-    def add_scalars(self, tag: Union[str, list], value: Union[float, int, list]):
-        if not isinstance(tag, list):
-            tag = [tag]
-            value = [value]
-        for idx, _ in enumerate(tag):
-            self.log["scalars"][tag[idx]] = value[idx]
-
-    def log_episode(self, timestep: int, train_reward: int, test_reward: int):
-        self.add_scalars(
-            ["Reward/Train_Reward", "Reward/Test_Reward"],
-            [train_reward, test_reward],
-        )
-        self.data_manager.update(self.log, timestep)
-
-    def send_log(self, timestep: int):
-        self.data_manager.update(self.log, timestep)
-
-    def add_replay_statistics(self, episodes: list):
-        n_episodes = len(episodes)
-        episode_returns = [ep["cum_rew"] for ep in episodes]
-        episode_lengths = [len(ep["actions"]) for ep in episodes]
-        total_lengths = sum(episode_lengths)
-        max_len = max(episode_lengths)
-        min_len = min(episode_lengths)
-        max_return = max(episode_returns)
-        avg_return = sum(episode_returns) / n_episodes
-        avg_len = total_lengths / n_episodes
-        self.add_scalars("Data/Avg Episode Length", avg_len)
-        self.add_scalars("Data/Max Episode Length", max_len)
-        self.add_scalars("Data/Min Episode Length", min_len)
-        self.add_scalars("Data/Avg Episode Return", avg_return)
-        self.add_scalars("Data/Max Episode Return", max_return)
-        self.add_scalars("Data/Number of Episodes", n_episodes)
-        self.add_scalars(
-            "Data/Buffer Size", sum([len(ep["states"]) for ep in episodes])
-        )
-        return min_len, max_len, total_lengths
-
-
 class LossLog:
     def __init__(self, name):
         self.n = 0
@@ -71,3 +27,52 @@ class LossLog:
             [f"Loss/{self.name}-Total", f"Loss/{self.name}-Average"],
             [self.total, self.total / self.n],
         )
+
+
+class Logger:
+    def __init__(self, data_manager: "DataManager"):
+        self.log = {"scalars": {}, "timestep_scalars": {}}
+        self.data_manager = data_manager
+
+    def add_scalars(self, tag: Union[str, list], value: Union[float, int, list]):
+        if not isinstance(tag, list):
+            tag = [tag]
+            value = [value]
+        for idx, _ in enumerate(tag):
+            self.log["scalars"][tag[idx]] = value[idx]
+
+    def log_episode(self, timestep: int, train_reward: int, test_reward: int):
+        self.add_scalars(
+            ["Reward/Train_Reward", "Reward/Test_Reward"],
+            [train_reward, test_reward],
+        )
+        self.data_manager.update(self.log, timestep)
+
+    def send_log(self, timestep: int):
+        self.data_manager.update(self.log, timestep)
+
+    def log_losses(self, *losses: LossLog):
+        for loss in losses:
+            names, values = loss.get_scalar()
+            self.add_scalars(names, values)
+
+    def add_replay_statistics(self, episodes: list):
+        n_episodes = len(episodes)
+        episode_returns = [ep["cum_rew"] for ep in episodes]
+        episode_lengths = [len(ep["actions"]) for ep in episodes]
+        total_lengths = sum(episode_lengths)
+        max_len = max(episode_lengths)
+        min_len = min(episode_lengths)
+        max_return = max(episode_returns)
+        avg_return = sum(episode_returns) / n_episodes
+        avg_len = total_lengths / n_episodes
+        self.add_scalars("Data/Avg Episode Length", avg_len)
+        self.add_scalars("Data/Max Episode Length", max_len)
+        self.add_scalars("Data/Min Episode Length", min_len)
+        self.add_scalars("Data/Avg Episode Return", avg_return)
+        self.add_scalars("Data/Max Episode Return", max_return)
+        self.add_scalars("Data/Number of Episodes", n_episodes)
+        self.add_scalars(
+            "Data/Buffer Size", sum([len(ep["states"]) for ep in episodes])
+        )
+        return min_len, max_len, total_lengths
